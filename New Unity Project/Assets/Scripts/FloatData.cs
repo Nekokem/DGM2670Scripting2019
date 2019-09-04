07a90bc4bf48e4685f89636c4de5500f908d047d
Katie <kamytherabbit@gmail.com>
2019-09-04 16:35:57 -0600

    Added more scripts

--- New Unity Project/Assets/Scripts/FloatData.cs
+++ New Unity Project/Assets/Scripts/FloatData.cs
@@ -1,15 +1,39 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 [CreateAssetMenu]
 public class FloatData : ScriptableObject
 {
 
     public float value = 1f;
-
+    public float maxValue = 1f;
     public void Updatevalue(float amount)
     {
         value += amount;
+    }
+
+
+    public void UpdatevalueLimitZero(float amount)
+    {
+        if (value < 0)
+        {
+            value = 0;
+        }
+        else
+        {
+            Updatevalue(amount);
+        }
+    }
+
+    public void UpdatevalueLimitZeroandMax(float amount)
+    {
+        if (value < maxValue)
+       {
+           Updatevalue(amount);
+       }
+       else
+       {
+           value = maxValue;
+       }
+        UpdatevalueLimitZero(amount);
     }
 }
---
+++ New Unity Project/Assets/Scripts/ImageControler.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Image))]
+public class ImageControler : MonoBehaviour
+{
+    private Image ImageComponent;
+    public UnityEvent UpdateImageEvnet;
+    private void Start()
+    {
+        ImageComponent = GetComponent<Image>();
+    }
+    
+    public void UpdateImageComponent(FloatData dataobj)
+    {
+        ImageComponent.fillAmount = dataobj.value;
+    }
+
+    private void Update()
+    {
+       UpdateImageEvnet.Invoke();
+    }
+}
